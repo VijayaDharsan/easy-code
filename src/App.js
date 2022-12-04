dd3e6bc5892bf1f584240560e897a2d79460cea5
balajiviy11 <balajivish2020@gmail.com>
2022-12-04 20:32:11 +0530

    Added some useState changes

--- src/App.js
+++ src/App.js
@@ -7,20 +7,24 @@ const App = () => {
   const [js, setJs] = useState("");
   const [srcDoc, setSrcDoc] = useState("");
 
-    const downloadTxtFile = (id) => {
-      console.log("called with id => ", id)
-      const correctFile = id == 1 ? html : id == 2 ? css : js
-      console.log("correct file ", correctFile)
 
-      const url = window.URL.createObjectURL(new Blob([correctFile]));
-      console.log("url download ", url)
-      const link = document.createElement('a');
-      link.href = url;
-      link.setAttribute('download', 'file.txt');
-      document.body.appendChild(link);
-      link.click();
-    }
-  
+  // This is for creating a Blob Object
+  // It will create a file for downloading 
+  // whichever the user want to compile 
+  const downloadTxtFile = (id) => {
+    console.log("called with id => ", id)  //You can know which type file will be downloaded
+    const correctFile = id == 1 ? html : id == 2 ? css : js 
+    console.log("correct file ", correctFile)
+
+    const url = window.URL.createObjectURL(new Blob([correctFile]));
+    console.log("url download ", url)
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute('download', 'file.txt');  //Till there is pdf and doc aren't working....
+    document.body.appendChild(link);
+    link.click();
+  }
+
 
   useEffect(() => {
     const timeout = setTimeout(() => {
